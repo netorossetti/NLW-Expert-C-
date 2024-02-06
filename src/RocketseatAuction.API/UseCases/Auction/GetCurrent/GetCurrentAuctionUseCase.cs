44b4f632e048dd449e60995364cdeb68d8b6b06e
Neto Rossetti <netinho.rossetti@gmail.com>
2024-02-06 10:12:06 -0300

    NLW - Aula 1

---
+++ src/RocketseatAuction.API/UseCases/Auction/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase {
+
+    public Auction? Execute() {
+        var repository = new RockeseatAuctionDbContext();
+
+        var today = DateTime.Now;
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => auction.Starts <= today && auction.Ends >= today);
+    }
+}
